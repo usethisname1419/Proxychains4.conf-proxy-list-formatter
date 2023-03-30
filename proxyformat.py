f28b856901472a1ef01b51411658348a8177f38f
usethisname1419 <101991363+usethisname1419@users.noreply.github.com>
2023-03-29 21:18:14 -0600

    Update proxyformat.py
    fixed major bug, still can't get auth to work

--- proxyformat.py
+++ proxyformat.py
@@ -1,10 +1,23 @@
+
 file = "proxys.txt"
-socks = "socks5  "
+print("Easy Proxy List Formatter - By Derek Johnston")
+print("Leave a space after each input")
+proto = input("Input Proxy Protocol:   ")
+socks = proto
 
 oauth = input("Enter Username and Password. If no auth leave blank:  ")
 
+
 with open(file, 'r') as fp:
-    ips = [socks.join(['', x.replace(':', ' '), oauth]) for x in fp.readlines()]
+    ips = [socks.join(['', x.replace(':', ' ')]) for x in fp.readlines()]
 
 with open(file, 'w') as fp:
+    ips.append(oauth)
     fp.writelines(ips)
+
+
+
+
+
+
+
